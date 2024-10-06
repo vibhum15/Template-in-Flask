bc26c53c6ca46c3950bf893be95b1b8d5dddf58c
Vibhum Gautam <20je1077@cve.iitism.ac.in>
2024-10-06 19:18:40 +0530

    block templete

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home():
-    return "Hello Mom"
+    myvalue = 'New value'
+    cal = 34*23
+    list = [1,20,3,55,3]
+    return render_template('index.html',list = list)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',debug=True )
