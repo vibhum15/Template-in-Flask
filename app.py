06aaf90466c306d7cb319316ac6e418c4df78d26
Vibhum Gautam <20je1077@cve.iitism.ac.in>
2024-10-07 20:31:22 +0530

    template complete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__, template_folder='templates')
 
@@ -9,5 +9,15 @@ def home():
     list = [1,20,3,55,3]
     return render_template('index.html',list = list)
 
+@app.route('/another')
+def another():
+    text = "Appling filters"
+    return render_template('another.html',text = text)
+
+@app.route('/go_to_another')
+def go_to_another():
+    return redirect(url_for('another'))
+    
+    
 if __name__ == '__main__':
     app.run(host='127.0.0.1',debug=True )
